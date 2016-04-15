0467ac8cd5cb13d2e057cf0b5fdbe5e3017c0ec8
kushagrjolly15 <kushagrjolly15@gmail.com>
2016-04-15 10:17:08 +0530

    changes in last activity

--- app/src/main/java/com/example/rahulthapar/e_qual/Activity_54_3.java
+++ app/src/main/java/com/example/rahulthapar/e_qual/Activity_54_3.java
@@ -13,8 +13,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-public class Activity_54 extends Activity {
-    RadioButton fifty4_1,fifty4_2,fifty4_3,fifty4_4,fifty4_5;
+public class Activity_54_3 extends Activity {
+    RadioButton fifty4_3_1,fifty4_3_2,fifty4_3_3,fifty4_3_4,fifty4_3_5;
     Button cont;
     private RadioGroup radiogrp;
     String choice = "";
@@ -25,21 +25,21 @@ public class Activity_54 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         data = getIntent().getStringExtra("data");
-        setContentView(R.layout.activity_activity_54);
-        fifty4_1 = (RadioButton)findViewById(R.id.radiobutton1_54);
-        fifty4_2 = (RadioButton)findViewById(R.id.radiobutton2_54);
-        fifty4_3 = (RadioButton)findViewById(R.id.radiobutton3_54);
-        fifty4_4 = (RadioButton)findViewById(R.id.radiobutton4_54);
-        fifty4_5 = (RadioButton)findViewById(R.id.radiobutton5_54);
-        cont = (Button)findViewById(R.id.save_next_act54);
-        radiogrp  = (RadioGroup)findViewById(R.id.radiogroup54);
+        setContentView(R.layout.activity_activity_54_3);
+        fifty4_3_1 = (RadioButton)findViewById(R.id.radiobutton1_54_3);
+        fifty4_3_2 = (RadioButton)findViewById(R.id.radiobutton2_54_3);
+        fifty4_3_3 = (RadioButton)findViewById(R.id.radiobutton3_54_3);
+        fifty4_3_4 = (RadioButton)findViewById(R.id.radiobutton4_54_3);
+        fifty4_3_5 = (RadioButton)findViewById(R.id.radiobutton5_54_3);
+        cont = (Button)findViewById(R.id.save_next_act54_3);
+        radiogrp  = (RadioGroup)findViewById(R.id.radiogroup54_3);
         cont.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 saveChoice();
-                data = data + "#" + choice ;
-                Toast.makeText(Activity_54.this, data, Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(Activity_54.this, Activity_54_2.class);
+                data = data + "#" + choice;
+                Toast.makeText(Activity_54_3.this, data, Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(Activity_54_3.this, Activity_55.class);
                 i.putExtra("data", data);
                 startActivity(i);
             }
@@ -49,19 +49,19 @@ public class Activity_54 extends Activity {
     public void saveChoice(){
         int id = radiogrp.getCheckedRadioButtonId();
         switch(id) {
-            case R.id.radiobutton1_54:
+            case R.id.radiobutton1_54_3:
                 choice = "1";
                 break;
-            case R.id.radiobutton2_54:
+            case R.id.radiobutton2_54_3:
                 choice = "2";
                 break;
-            case R.id.radiobutton3_54:
+            case R.id.radiobutton3_54_3:
                 choice = "3";
                 break;
-            case R.id.radiobutton4_54:
+            case R.id.radiobutton4_54_3:
                 choice = "4";
                 break;
-            case R.id.radiobutton5_54:
+            case R.id.radiobutton5_54_3:
                 choice = "5";
                 break;
 
--- app/src/main/java/com/example/rahulthapar/e_qual/Activity_55.java
+++ app/src/main/java/com/example/rahulthapar/e_qual/Activity_55.java
@@ -28,9 +28,9 @@ public class Activity_55 extends Activity {
     private RadioGroup radiogrp,radiogrp1;
     String choice = "";
     String choice1 = "";
+    String choice2="";
+    String choice3="";
     String data;
-    EditText pinn,pinn1;
-    String pin="",pin1="";
     private FileWriter mFileWriter;
 
     @Override
@@ -45,17 +45,14 @@ public class Activity_55 extends Activity {
         fifty8_3 = (RadioButton)findViewById(R.id.radiobutton3_55);
         cont = (Button)findViewById(R.id.save_next_act55);
         cancel = (Button)findViewById(R.id.cancel_55);
-        pinn = (EditText)findViewById(R.id.edit1_55);
-        pinn1 = (EditText)findViewById(R.id.edit2_55);
+
         radiogrp  = (RadioGroup)findViewById(R.id.radiogroup55);
         radiogrp1  = (RadioGroup)findViewById(R.id.radiogroup58);
         cont.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 saveChoice();
-                pin = pinn.getText().toString();
-                pin1 = pinn1.getText().toString();
-                data = data + "#" + choice + "#" + pin + "#" + pin1 + "#" + choice1;
+                data = data + "#" + choice + "#" + choice2 + "#" + choice3 + "#" + choice1;
                 Toast.makeText(Activity_55.this, data, Toast.LENGTH_SHORT).show();
                 try {
                     saveFile();
@@ -79,7 +76,7 @@ public class Activity_55 extends Activity {
         String[] row=data.split("#");
         String[] ques={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22",
                 "23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40",
-                "41","42","43","44","45","46","47","48","49","50","51","52","53","54","55"};
+                "41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58"};
         String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
         String fileName = "E-Qual.csv";
         String filePath = baseDir + File.separator + fileName;
@@ -110,6 +107,19 @@ public class Activity_55 extends Activity {
                 break;
             case R.id.no55:
                 choice = "2";
+                break;
+            case R.id.yes56:
+                choice2 = "1";
+                break;
+
+            case R.id.no56:
+                choice2 = "2";
+                break;
+            case R.id.yes57:
+                choice3 = "1";
+                break;
+            case R.id.no57:
+                choice3 = "2";
                 break;
         }
         switch(id1) {
